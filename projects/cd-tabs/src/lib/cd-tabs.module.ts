8b399d0c949536f6b186b5e1e1d28d022273cf52
Clement G <contact@clemdesign.fr>
2019-12-30 21:18:54 +0100

    Add TODO

--- projects/cd-tabs/src/lib/cd-tabs.module.ts
+++ projects/cd-tabs/src/lib/cd-tabs.module.ts
@@ -6,6 +6,8 @@ import {CdTabButtonComponent} from './cd-tab-button.component';
 import {CdTabContentComponent} from './cd-tab-content.component';
 import {RouterModule} from '@angular/router';
 
+// TODO: To run "ionic build --prod", rename the library "cd-tabs" as "angular-cd-tabs" all needed place
+
 
 @NgModule({
     declarations: [
